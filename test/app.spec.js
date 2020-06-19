729e7c98b8861027e2d0aefec7674e77665bf995
Alvin Der <fulmar38251@letterboxes.org>
2020-06-18 21:54:21 -0400

    added smallTruck test

--- test/app.spec.js
+++ test/app.spec.js
@@ -8,8 +8,12 @@ const app = require('../app.js');
 describe('Vehicle Rating Calculator', function() {
     it('calculate a rating of 10 for a car with 4 cylinters and diesel fuel', function () {
         const smallCar = app.smallCar;
-        const smallTruck = app.smallTruck;
         const rating = app.calculateVehicleRating(smallCar);
+        assert.equal(rating, 10);
+    } );
+    it('calculate a rating for a truck with 8 cylinters and regular fuel', function () {
+        const smallTruck = app.smallTruck;
+        const rating = app.calculateVehicleRating(smallTruck);
         assert.equal(rating, 10);
     } );
 } );
